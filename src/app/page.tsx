ee41adb595b83949d72738104b5949d4aa31f287
Vietson <vietson@gmail.com>
2023-04-20 16:34:56 -0400

    update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-6xl font-bold leading-normal"><a href="/">LOUER</a></h1>
-      <h2 className="text-3xl font-bold leading-normal">A Real Estate Platform For Brokers, Agents, Investors</h2>
+      <h2 className="text-3xl font-bold leading-normal">A Real Estate Platform For Brokers, Agents, Investors, Homeowners, and Buyers</h2>
       <h3 className='text-xl font-bold leading-normal'>COMING SOON!</h3>
     </main >
   )
